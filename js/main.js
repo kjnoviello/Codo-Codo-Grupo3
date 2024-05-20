f073e909bd29166e004c3c5c9e49548d4fe1a36a
Yago <104085125+YagoGomez83@users.noreply.github.com>
2024-05-19 23:55:01 -0300

    pagina nosotros creada

--- js/main.js
+++ js/main.js
@@ -1,22 +1,13 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  var map = L.map("map").setView([-34.601242, -58.392862], 13); // Coordenadas para la Avenida 9 de Julio 2444
-
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  }).addTo(map);
-
-  var marker = L.marker([-34.601242, -58.392862], { draggable: true }).addTo(
-    map
-  ); // Crear un marcador en la ubicación deseada
-  marker.bindPopup("Avenida 9 de Julio 2444, Buenos Aires").openPopup();
-
-  marker.on("dragend", function (event) {
-    // Evento para actualizar la posición del marcador al arrastrarlo
-    var marker = event.target;
-    var position = marker.getLatLng();
-    map.panTo(new L.LatLng(position.lat, position.lng));
+  const check = document.querySelector("#terms");
+  const buttom = document.querySelector("#submit");
+  check.addEventListener("change", () => {
+    if (check.checked) {
+      buttom.disabled = false;
+    } else {
+      buttom.disabled = true;
+    }
   });
 });
 
