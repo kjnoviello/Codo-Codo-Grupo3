10d3078453c06620f597352b28674a07cb8520d2
kevin joel noviello <kjnoviello@gmail.com>
2024-05-20 11:34:22 -0300

    several fixes and upgrades

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+// display del mapa en contacto
+
+document.addEventListener("DOMContentLoaded", function () {
+  var map = L.map("map").setView([-34.601242, -58.392862], 13); // Coordenadas para la Avenida 9 de Julio 2444
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  var marker = L.marker([-34.601242, -58.392862], { draggable: false }).addTo(
+    map
+  ); // Crear un marcador en la ubicación deseada
+  marker.bindPopup("Avenida 9 de Julio 2444, Buenos Aires").openPopup();
+
+  marker.on("dragend", function (event) {
+    // Evento para actualizar la posición del marcador al arrastrarlo
+    var marker = event.target;
+    var position = marker.getLatLng();
+    map.panTo(new L.LatLng(position.lat, position.lng));
+  });
+
+});
+
+
+// toogle boton surscibir en nosotros
 
 document.addEventListener("DOMContentLoaded", function () {
   const check = document.querySelector("#terms");
@@ -11,7 +37,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-const bodyEl = document.body;
+
+// toogle menu hamburguesa en responsive
+
 const hamburgerEl = document.querySelector('.nav__button-menu');
 const menuEl = document.getElementById('nav__menu');
 
