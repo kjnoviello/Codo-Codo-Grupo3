13f10a4a602cb6a38b320709aeb33d64253f9bff
kevin joel noviello <kjnoviello@gmail.com>
2024-05-22 09:36:51 -0300

    add modal de reservacion

--- js/main.js
+++ js/main.js
@@ -47,3 +47,17 @@ hamburgerEl.addEventListener('click', () => {
     menuEl.classList.toggle('active');
 
 });
+
+
+// toggle modal reservacion
+
+const btnReservacionResp = document.querySelector('.openReservacionResp');
+const btnReservacion = document.querySelector('.openReservacion');
+const showReservacion = document.getElementById('openReservacion');
+const btnCloseReservation = document.getElementById('closeReservation');
+
+
+btnReservacionResp.addEventListener('click', ()=> { showReservacion.classList.toggle('hideReservacion')});
+btnReservacion.addEventListener('click', ()=> { showReservacion.classList.toggle('hideReservacion')});
+btnCloseReservation.addEventListener('click', ()=>{  showReservacion.classList.toggle('hideReservacion')})
+
