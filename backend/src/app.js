47f038385d920a4c4f9eecaaf61b79f744a2041b
kevin joel noviello <kjnoviello@gmail.com>
2024-07-12 10:47:14 -0300

    final backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ const express = require("express");
 
 const app = express();
 
-// const reservationRoutes = require("./routes/reservationController");
 const userRoutes = require("../routes/userRoutes");
 const reservationRoutes = require("../routes/reservationRoutes");
 
@@ -13,11 +12,6 @@ const PORT = 3000;
 app.use(express.json());
 app.use("/clientes", userRoutes);
 app.use("/clientes", reservationRoutes);
-// app.use("/reservation", reservationRoutes);
-
-//ruta  principal es clientes
-// app.use('/clientes', reservasRoutes);
-// app.use('/clientes', usuariosRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto: ${PORT}`);
